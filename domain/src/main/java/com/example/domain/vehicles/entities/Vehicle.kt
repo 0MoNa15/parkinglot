7edc7da130667e3f107c1b9105ca29be4cbc171d
CEIBA\zorayda.gutierrez <zorayda.gutierrez@ceiba.com.co>
2021-01-07 22:10:20 -0500

    Se dejan las constantes con un modificador de acceso público

--- domain/src/main/java/com/example/domain/vehicles/entities/Vehicle.kt
+++ domain/src/main/java/com/example/domain/vehicles/entities/Vehicle.kt
@@ -2,8 +2,10 @@ package com.example.domain.vehicles.entities
 
 abstract class Vehicle {
     // Para el estado del vehiculo
-    val INSIDE_PARKING_LOT = "inside_the_parking_lot"
-    val OUTSIDE_PARKING_LOT = "outside_the_parking_lot"
+    companion object {
+        val INSIDE_PARKING_LOT = "inside_the_parking_lot"
+        val OUTSIDE_PARKING_LOT = "outside_the_parking_lot"
+    }
 
     abstract val plateLicensePlate: LicensePlate
     abstract var model: String
