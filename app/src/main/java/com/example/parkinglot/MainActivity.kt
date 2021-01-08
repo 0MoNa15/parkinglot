ae7c9b1fcebddcb8db3be5a91fe6b3a5c7ff6a73
CEIBA\zorayda.gutierrez <zorayda.gutierrez@ceiba.com.co>
2021-01-08 02:11:08 -0500

    Cambio del logo

--- app/src/main/java/com/example/parkinglot/MainActivity.kt
+++ app/src/main/java/com/example/parkinglot/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.parkinglot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.infrastructure.database.dto.CarDao
 import com.example.infrastructure.database.dto.MotorcycleDao
 import dagger.hilt.android.AndroidEntryPoint
@@ -9,9 +11,25 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    lateinit var recyclerVehicle: RecyclerView
+    //private lateinit var bindingMainActivity: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /*bindingMainActivity = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMainActivity.root
+        */
         setContentView(R.layout.activity_main)
+        initialiceComponents()
+    }
+
+    private fun initialiceComponents() {
+
+        //RecyclerView
+        /*recyclerVehicle = bindingMainActivity.recyclerViewSearchResults
+        recyclerVehicle.layoutManager = GridLayoutManager(this@MainActivity.applicationContext)
+        */
+
     }
 }
