df4307c4eb8e49ad1913c64cf7fddabbd0fc0fe4
CEIBA\zorayda.gutierrez <zorayda.gutierrez@ceiba.com.co>
2021-01-07 22:09:44 -0500

    Se agrega el valor de dia y hora. También variables para rectificar parámetros especiales para el core de negocio para las motos

--- domain/src/main/java/com/example/domain/vehicles/aggregates/Motorcycle.kt
+++ domain/src/main/java/com/example/domain/vehicles/aggregates/Motorcycle.kt
@@ -9,4 +9,11 @@ class Motorcycle(
     override var color: String,
     override var state: String,
     val cylinderCapacity: Int
-): Vehicle()
+): Vehicle(){
+    companion object {
+        const val COST_PER_HOUR_MOTORCYCLE = 500
+        const val COST_PER_DAY_MOTORCYCLE = 4000
+        const val ADITIONAL_COST_FOR_LARGER_CYLINDER_CAPACITY = 200
+        const val SPECIAL_CYLINDER_CAPACITY_FROM = 500
+    }
+}
