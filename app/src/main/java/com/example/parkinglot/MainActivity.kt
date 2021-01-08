f21ae57d83f794c4c38be4e56534784c17769534
CEIBA\zorayda.gutierrez <zorayda.gutierrez@ceiba.com.co>
2021-01-08 02:17:57 -0500

    Creción de instancias con viewbinding

--- app/src/main/java/com/example/parkinglot/MainActivity.kt
+++ app/src/main/java/com/example/parkinglot/MainActivity.kt
@@ -6,27 +6,27 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.infrastructure.database.dto.CarDao
 import com.example.infrastructure.database.dto.MotorcycleDao
+import com.example.parkinglot.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     lateinit var recyclerVehicle: RecyclerView
-    //private lateinit var bindingMainActivity: ActivityMainBinding
+    private lateinit var bindingMainActivity: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        /*bindingMainActivity = ActivityMainBinding.inflate(layoutInflater)
-        val view = bindingMainActivity.root
-        */
-        setContentView(R.layout.activity_main)
         initialiceComponents()
     }
 
     private fun initialiceComponents() {
+        // Configuración
+        bindingMainActivity = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMainActivity.root
+        setContentView(view)
 
-        //RecyclerView
+        // Lista
         /*recyclerVehicle = bindingMainActivity.recyclerViewSearchResults
         recyclerVehicle.layoutManager = GridLayoutManager(this@MainActivity.applicationContext)
         */
