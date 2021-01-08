9a1ff610275aa2c3871e5bd5ebd79accea93ebe1
CEIBA\zorayda.gutierrez <zorayda.gutierrez@ceiba.com.co>
2021-01-07 21:30:44 -0500

    Modificación de parámetros y eliminación de datos no necesarios

--- domain/src/main/java/com/example/domain/parkinglot/valueobjects/Day.kt
+++ domain/src/main/java/com/example/domain/parkinglot/valueobjects/Day.kt
@@ -1,19 +1,44 @@
 package com.example.domain.parkinglot.valueobjects
 
+import java.util.*
+import kotlin.collections.ArrayList
+
 class Day {
-    val name: String? = null
-    val type: TypeOfDay? = null
+    var identifyDay: Int? = null
+    var type: TypeOfDay? = null
 
     enum class TypeOfDay {
-        NORMAL_DAY{
-            val motorcycleCost = 4000
-            val carCost = 8000
-        },
+        // Solo en los dias especiales pueden ingresar los vehiculos con placa que inicie en 'A'
+        SPECIAL_DAYD,
+        NORMAL_DAY
+    }
+    
+    companion object {
+        fun availablesDays(): List<Day>{
+            val listDays: List<Day> = ArrayList()
 
-        // Solo en estos dias pueden ingresar los vehiculos con placa que inicie en 'A'
-        SPECIAL_DAYD{
-            val motorcycleCost = 4000
-            val carCost = 8000
+            listDays[0].identifyDay = Calendar.MONDAY
+            listDays[0].type = TypeOfDay.SPECIAL_DAYD
+
+            listDays[1].identifyDay = Calendar.TUESDAY
+            listDays[1].type = TypeOfDay.NORMAL_DAY
+
+            listDays[2].identifyDay = Calendar.WEDNESDAY
+            listDays[2].type = TypeOfDay.NORMAL_DAY
+
+            listDays[3].identifyDay = Calendar.THURSDAY
+            listDays[3].type = TypeOfDay.NORMAL_DAY
+
+            listDays[4].identifyDay = Calendar.FRIDAY
+            listDays[4].type = TypeOfDay.NORMAL_DAY
+
+            listDays[5].identifyDay = Calendar.SATURDAY
+            listDays[5].type = TypeOfDay.NORMAL_DAY
+
+            listDays[6].identifyDay = Calendar.SUNDAY
+            listDays[6].type = TypeOfDay.SPECIAL_DAYD
+
+            return listDays
         }
     }
 }
