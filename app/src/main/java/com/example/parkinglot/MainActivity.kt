dc14436488d82ae6de42eac04bae93815abc4139
CEIBA\zorayda.gutierrez <zorayda.gutierrez@ceiba.com.co>
2021-01-08 01:24:19 -0500

    Anotación para ir convirtiendo al Main Activity en una dependencia

--- app/src/main/java/com/example/parkinglot/MainActivity.kt
+++ app/src/main/java/com/example/parkinglot/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.parkinglot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.infrastructure.database.dto.CarDao
+import com.example.infrastructure.database.dto.MotorcycleDao
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
