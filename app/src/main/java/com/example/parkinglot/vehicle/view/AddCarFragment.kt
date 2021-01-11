82948a5b5c8496c2f32034a6a5b3e88bafc21f26
CEIBA\zorayda.gutierrez <zorayda.gutierrez@ceiba.com.co>
2021-01-11 01:01:44 -0500

    Se eliminan importaciones que no se utilizan

--- app/src/main/java/com/example/parkinglot/vehicle/view/AddCarFragment.kt
+++ app/src/main/java/com/example/parkinglot/vehicle/view/AddCarFragment.kt
@@ -1,12 +1,10 @@
 package com.example.parkinglot.vehicle.view
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.parkinglot.R
 import com.example.parkinglot.vehicle.viewmodel.AddCarViewModel
