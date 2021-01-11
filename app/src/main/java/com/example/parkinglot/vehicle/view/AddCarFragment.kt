09982764b66fcd64683b58a946cc49c1e2a49a8c
CEIBA\zorayda.gutierrez <zorayda.gutierrez@ceiba.com.co>
2021-01-11 01:24:53 -0500

    Se crea instanciación de los componentes gráficos de la vista ara agregar un vehículo

--- app/src/main/java/com/example/parkinglot/vehicle/view/AddCarFragment.kt
+++ app/src/main/java/com/example/parkinglot/vehicle/view/AddCarFragment.kt
@@ -5,19 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
 import androidx.lifecycle.ViewModelProvider
 import com.example.parkinglot.R
+import com.example.parkinglot.databinding.AddCarFragmentBinding
 import com.example.parkinglot.vehicle.viewmodel.AddCarViewModel
 
 class AddCarFragment : Fragment() {
-
     private lateinit var viewModel: AddCarViewModel
+    private lateinit var bindingAddCarFragment: AddCarFragmentBinding
+    lateinit var savedVehicleButton: Button
+    lateinit var licensePlateEditText: EditText
+    lateinit var carTypeSwitch: Switch
+    lateinit var motorcycleTypeSwitch: Switch
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.add_car_fragment, container, false)
+        bindingAddCarFragment = AddCarFragmentBinding.inflate(layoutInflater)
+        initialiceWidget(bindingAddCarFragment.root)
+        return bindingAddCarFragment.root
+    }
+
+    private fun initialiceWidget(view: View) {
+        carTypeSwitch = view.findViewById(R.id.switchCarType)
+        motorcycleTypeSwitch = view.findViewById(R.id.switchMotorcycleType)
+        licensePlateEditText = view.findViewById(R.id.editTextLicensePlate)
+        savedVehicleButton = view.findViewById(R.id.buttonAddCar)
+        savedVehicleButton.setOnClickListener {
+            savedVehicleButton.alpha = 1.0F
+            //savedVehicleButton.alpha = 0.5F
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
