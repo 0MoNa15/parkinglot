31bcacde97c71efc146075dfcba6df2ab30ceeb8
CEIBA\zorayda.gutierrez <zorayda.gutierrez@ceiba.com.co>
2021-01-12 02:56:29 -0500

    Se agrega método que permite convertir la fecha actual en milisegundos

--- app/src/main/java/com/example/parkinglot/generic/Utils.kt
+++ app/src/main/java/com/example/parkinglot/generic/Utils.kt
@@ -1,9 +1,19 @@
 package com.example.parkinglot.generic
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 class Utils {
     companion object {
         fun validateString(text: String?): Boolean{
             return text != null && text.isNotEmpty()
+        }
+
+        fun convertDateInMilliseconds(): Long{
+            val myDate = Date().toString()
+            val sdf = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+            val date: Date = sdf.parse(myDate)
+            return date.time
         }
     }
 }
